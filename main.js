a8abbbeaf875d84481727cc314350ecc9794f3bd
Andrew Green <green.andrew.b@gmail.com>
2020-01-24 13:45:27 -0600

    added features to clear input fields, reset focus, and window alert user if either field left empty

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 const container = document.getElementById("container")
 
 document.getElementById("saveToWishlist").addEventListener("click", event => {
+    if (document.getElementById("thing").value === ""
+        ||
+        document.getElementById("place").value === "") {
+    alert("Ya left a field blank ya dingus!")
+} else {
 
     const thing = document.getElementById("thing").value
     const place = document.getElementById("place").value
 
+    /*
+    clears DOM if desired 
+    container.innerHTML = ""
+    */
+    
     container.innerHTML += `
         <section>
             <h1>I can purchase ${thing} at ${place}</h1>
         </section>
     `
+    document.getElementById("thing").value = "";
+    document.getElementById("place").value = "";
+    document.getElementById("thing").focus();
+};
 })
